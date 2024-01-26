d4483a4975837f95ccd4bb5a7e5cc8c0f8f00c3f
tmccf1570381 <makoto_tanabe@mail.toyota.co.jp>
2024-01-26 18:34:11 +0900

    マイページにローディング追加

--- src/components/Contents/MyPage/MyPage.tsx
+++ src/components/Contents/MyPage/MyPage.tsx
@@ -3,23 +3,30 @@ import { UserContext } from "../../../App"
 import styles from "../../../css/MyPage.module.css"
 import { examLogType } from "../../../model/VariableContextProps";
 import ExamLogTable from "./ExamLogTable";
+import { VariableContext } from "../../../contexts/VariableContextProvider";
+import Loading from "../../Loading/Loading";
 
 export default function MyPage () {
     const { user } = useContext(UserContext);
+    const { isLoading, setIsLoading } = useContext(VariableContext);
     const [examLog, setExamLog] = useState<examLogType>([]);
-    const [a,setA] = useState({acc:0, sum:1})
+    const [tempArr,setTempArr] = useState({acc:0, sum:1})
     
     useEffect(()=>{
         (async()=>{
+            setIsLoading(true);
             const log = await fetch("https://lishd6r5ff.execute-api.ap-northeast-1.amazonaws.com/prod/api/v1/results")
             .then(e=>e.json());
             setExamLog(log.Items);
-            const summary = log.Items.map((e:any)=>e.ansList).flat()
-            setA({acc:summary.reduce((sum:any, element:any)=>sum + element, 0),sum:summary.length})
+            const summary = log.Items.map((e:any)=>e.ansList).flat();
+            setTempArr({acc:summary.reduce((sum:any, element:any)=>sum + element, 0),sum:summary.length});
+            setIsLoading(false);
         })()
     },[])
 
     return (
+        <>
+        {isLoading && <Loading/>}
         <div className={styles.mypageArea}>
             <h2 className={styles.head}>{user.family_name+" "+user.given_name} <span className={styles.supplement}>さんのページ</span></h2>
             <h4>結果サマリ 💬</h4>
@@ -27,16 +34,17 @@ export default function MyPage () {
                 <div className={styles.headSectionCard}>
                     <span>Accuracy</span>
                     <span className={styles.accuracy}>
-                        {a.sum!==0 ? Math.floor((a.acc/a.sum)*100): 0} %
+                        {tempArr.sum!==0 ? Math.floor((tempArr.acc/tempArr.sum)*100): 0} %
                     </span>
                 </div>
                 <div className={styles.headSectionCard}>
                     <span>Questions</span>
-                    <span className={styles.resolved}>{a.sum} 問</span>
+                    <span className={styles.resolved}>{tempArr.sum} 問</span>
                 </div>
             </section>
             <h4>学習履歴 📝</h4>
             <ExamLogTable examLog={examLog!} />
         </div>
+        </>
     )
 }
